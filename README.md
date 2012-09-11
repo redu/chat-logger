d787284952c8bbac21a8365f71d6331e7c3603f2
embs <embs@cin.ufpe.br>
2012-09-11 15:58:50 -0300

    rails new chat-logger

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat-Logger
+Uma aplicação para visualização de dados de conversação entre mentores e mentorandos
+do programa eMentoring - implantado sobre a plataforma Redu.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatLogger::Application.config.secret_token = '1a0f90d404967dd638f62415312834df88dd2fc733770d3d651a147436ea64542a1806d7d87cc4f892d4b5a3158ba1a532023eb15468f13326a1b3743ac46f9c'
